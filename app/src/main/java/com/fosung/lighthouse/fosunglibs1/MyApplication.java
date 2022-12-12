3a0c2c1e1efc5ce0a8fd866c5703e3c7af528095
liangxiao6 <liangxiao6@live.com>
2022-12-12 09:26:42 +0800

    merger

--- app/src/main/java/com/fosung/lighthouse/fosunglibs1/MyApplication.java
+++ app/src/main/java/com/fosung/lighthouse/fosunglibs1/MyApplication.java
@@ -1,36 +1,36 @@
-package com.fosung.lighthouse.fosunglibs1;
-
-import android.app.Application;
-
-import com.blankj.utilcode.util.SPUtils;
-import com.blankj.utilcode.util.Utils;
-import com.geek.libutils.data.MmkvUtils;
-
-import me.jessyan.autosize.AutoSize;
-import me.jessyan.autosize.AutoSizeConfig;
-import me.jessyan.autosize.unit.Subunits;
-
-
-/**
- * @author:wangshouxue
- * @date:2022/3/28 下午4:09
- * @description:类作用
- */
-public class MyApplication extends Application {
-    @Override
-    public void onCreate() {
-        super.onCreate();
-//        ApplicationUtils.init(this);
-        //
-        Utils.init(Utils.getApp());// com.blankj:utilcode:1.17.3
-        MmkvUtils.getInstance().get("");
-        MmkvUtils.getInstance().get_demo();
-        AutoSize.initCompatMultiProcess(this);
-        AutoSizeConfig.getInstance()
-                .setPrivateFontScale(SPUtils.getInstance().getFloat("textSizef", 0f))
-                .setExcludeFontScale(true)
-                .getUnitsManager()
-                .setSupportDP(true)
-                .setSupportSubunits(Subunits.MM);
-    }
-}
+//package com.fosung.lighthouse.fosunglibs1;
+//
+//import android.app.Application;
+//
+//import com.blankj.utilcode.util.SPUtils;
+//import com.blankj.utilcode.util.Utils;
+//import com.geek.libutils.data.MmkvUtils;
+//
+//import me.jessyan.autosize.AutoSize;
+//import me.jessyan.autosize.AutoSizeConfig;
+//import me.jessyan.autosize.unit.Subunits;
+//
+//
+///**
+// * @author:wangshouxue
+// * @date:2022/3/28 下午4:09
+// * @description:类作用
+// */
+//public class MyApplication extends Application {
+//    @Override
+//    public void onCreate() {
+//        super.onCreate();
+////        ApplicationUtils.init(this);
+//        //
+//        Utils.init(Utils.getApp());// com.blankj:utilcode:1.17.3
+//        MmkvUtils.getInstance().get("");
+//        MmkvUtils.getInstance().get_demo();
+//        AutoSize.initCompatMultiProcess(this);
+//        AutoSizeConfig.getInstance()
+//                .setPrivateFontScale(SPUtils.getInstance().getFloat("textSizef", 0f))
+//                .setExcludeFontScale(true)
+//                .getUnitsManager()
+//                .setSupportDP(true)
+//                .setSupportSubunits(Subunits.MM);
+//    }
+//}
